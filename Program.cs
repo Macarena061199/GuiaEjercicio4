0439a1bc70bda55c871203fb5e5762fb09ecb580
Macarena061199 <102336411+Macarena061199@users.noreply.github.com>
2022-04-02 16:34:04 -0300

    Listo

--- Program.cs
+++ Program.cs
@@ -4,3 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+bool flag = false;
+
+do
+{
+    Console.Write("Por favor ingrese una frase: ");
+    string frase = Console.ReadLine();
+    Console.WriteLine("Usted escribió " + frase);
+    string fin = "fin";
+
+    Console.WriteLine("¿Quiere seguir escribiendo frases? En caso negativo ingrese 'fin'.");
+    string frase2 = Console.ReadLine();
+    if (frase2 != fin)
+    {
+        flag = true;
+    }
+    else
+    {
+        Console.WriteLine("Usted escribió " + frase2 + ". En segundos el programa se cerrará.");
+        flag = false;
+    }
+}while(flag ==true);
